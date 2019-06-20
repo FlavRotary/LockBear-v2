7f1483650fb65b5aae6a7a8fbaec4c8e11f19eb6
Flavian Rotaru <stewietm85252@gmail.com>
2019-06-20 15:37:02 +0300

    Added AES + Keychain

--- Classes/Helpers/Managers/KeychainManager.swift
+++ Classes/Helpers/Managers/KeychainManager.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import KeychainSwift
+
+class KeychainAndEncryption{
+    static func createAndStore(){
+        do {
+            let password: String = String.generateRandomString(with: 3, true, true, true)
+            let dataFromPass = password.data(using: .utf8)
+            let salt = AES256Crypter.randomSalt()
+            let key = try AES256Crypter.createKey(password: dataFromPass!, salt: salt)
+            let keychain = KeychainSwift()
+            keychain.set(key, forKey: "AESKey", withAccess: KeychainSwiftAccessOptions.accessibleWhenUnlockedThisDeviceOnly)
+        } catch {
+            print("Failed To Create Key. Check for Errors!\n")
+            print(error)
+        }
+    }
+    
+    static func ecryptWithAES(_ sourceString:String) -> (Data?,Data?){
+        do{
+            let keychain = KeychainSwift()
+            let key = keychain.getData("AESKey")
+            let dataToEncrypt = sourceString.data(using: .utf8)
+            let iv = AES256Crypter.randomIv()
+            let aes = try AES256Crypter(key: key!, iv: iv)
+            let encryptedData = try aes.encrypt(dataToEncrypt!)
+            return (encryptedData,iv)
+            
+        } catch {
+            print("Failed the operation of Encryption. Check for errors! \n")
+            print(error)
+            return (nil,nil)
+        }
+    }
+    
+    static func decryptforAES(_ givenData: Data, _ givenIv: Data) -> String?{
+        do{
+            let keychain = KeychainSwift()
+            let key = keychain.getData("AESKey")
+            let aes = try AES256Crypter(key: key!, iv: givenIv)
+            let decryptedData = try aes.decrypt(givenData)
+            let resultedString = String(data: decryptedData, encoding: String.Encoding.utf8)
+            return resultedString!
+        } catch {
+            print("Failed the operation of Decryption. Check for Errors!\n")
+            return nil
+        }
+    }
+}
